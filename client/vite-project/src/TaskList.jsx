40274777c3d41bd66884735a73f00530754b23f9
thejjwang <jonathanjacobwang@gmail.com>
2023-10-06 12:02:04 -0700

    tasks display on homepage

--- client/vite-project/src/TaskList.jsx
+++ client/vite-project/src/TaskList.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const TaskList = ({tasks}) => {
+const TaskList = ({ tasks }) => {
   return (
     <div>
-        {tasks.map((task) => {
+      {tasks.map((task) => (
         <li key={task.id}>
-            <h2>{task.title}</h2>
-            <h3>{task.description}</h3>
-            <p>{task.status}</p>
-        </li>})
-    }
+          <h2>{task.title}</h2>
+          <h3>{task.description}</h3>
+          <p>{task.status}</p>
+        </li>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
